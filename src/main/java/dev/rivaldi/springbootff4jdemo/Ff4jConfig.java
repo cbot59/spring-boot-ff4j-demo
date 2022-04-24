70518482623b08c3954086af27b6d1d0bb4033bb
Rivaldi <cbot59@gmail.com>
2022-04-24 12:49:01 +0700

    fix: set audit to event based as it has issue on programmatically created FF4J feature
    programmatically created feature is sending event which requires security context, which is null when application is starting up and leads to NPE

--- src/main/java/dev/rivaldi/springbootff4jdemo/Ff4jConfig.java
+++ src/main/java/dev/rivaldi/springbootff4jdemo/Ff4jConfig.java
@@ -3,8 +3,12 @@ package dev.rivaldi.springbootff4jdemo;
 import javax.annotation.PostConstruct;
 
 import org.ff4j.FF4j;
+import org.ff4j.security.AuthorizationsManager;
+import org.ff4j.security.SpringSecurityAuthorisationManager;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 
 @Configuration
 class Ff4jConfig {
@@ -12,9 +16,16 @@ class Ff4jConfig {
     @Bean
     FF4j ff4j() {
         FF4j ff4j = new FF4j();
-        ff4j.audit(true);
         ff4j.autoCreate(true);
+        ff4j.setAuthorizationsManager(new SpringSecurityAuthorisationManager());
 
         return ff4j;
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    FF4j toggleAudit() {
+        ff4j().audit(true);
+
+        return ff4j();
     }
 }
