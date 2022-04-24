8d83184e959902bcfe7c2ec34baa5d7a287826a5
Rivaldi <cbot59@gmail.com>
2022-04-24 13:36:20 +0700

    feat: add SecurityConfig

---
+++ src/main/java/dev/rivaldi/springbootff4jdemo/SecurityConfig.java
@@ -0,0 +1,37 @@
+package dev.rivaldi.springbootff4jdemo;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/ff4j-web-console/**").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .and().formLogin().disable()
+            .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails customer = User.builder()
+            .authorities("customer")
+            .username("customer")
+            .password("{noop}customer")
+            .build();
+        UserDetails seller = User.builder()
+            .authorities("seller")
+            .username("seller")
+            .password("{noop}seller")
+            .build();
+        auth.inMemoryAuthentication()
+            .withUser(customer)
+            .withUser(seller);
+    }
+}
