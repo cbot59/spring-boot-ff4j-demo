2e410f5d322785d3fd2d31d53ed5d6c818574b52
Rivaldi <cbot59@gmail.com>
2022-04-24 14:43:56 +0700

    feat: disable spring security on favicon and disable csrf on ff4j web console

--- src/main/java/dev/rivaldi/springbootff4jdemo/SecurityConfig.java
+++ src/main/java/dev/rivaldi/springbootff4jdemo/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.rivaldi.springbootff4jdemo;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,9 +11,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/favicon.ico");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/ff4j-web-console/**").permitAll()
+            .and().csrf(csrf -> csrf.ignoringAntMatchers("/ff4j-web-console/**"))
+            .authorizeRequests()
             .antMatchers("/api/**").authenticated()
             .and().formLogin().disable()
             .httpBasic();
